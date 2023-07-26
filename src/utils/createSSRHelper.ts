c959cb284d7902256af0728144ea291c3a04985c
Filip Nystrand <filip.nystrand@gmail.com>
2023-07-26 22:42:48 +0200

    Added SSR helper

---
+++ src/utils/createSSRHelper.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../server/db";
+import { getSession } from "next-auth/react";
+import { appRouter } from "../server/api/root";
+import superjson from "superjson";
+import { createServerSideHelpers } from '@trpc/react-query/server';
+
+
+export const createSSRHelper = async () => (
+  createServerSideHelpers({
+  router: appRouter,
+  ctx: {
+    session: await getSession(),
+    prisma: prisma,
+    cronKey: '',
+  },
+  transformer: superjson, // optional - adds superjson serialization
+}));
