b134cd64d2249c8c0400e8f4f126bc107884f7c0
Filip Nystrand <filip.nystrand@gmail.com>
2023-08-21 21:07:40 +0200

    Updated Email template

--- src/components/emails/EventSignUp.tsx
+++ src/components/emails/EventSignUp.tsx
@@ -1,29 +1,26 @@
-import { VastraEvent, type Participant } from "@prisma/client";
+import { VastraEvent, type Participant, Bus } from "@prisma/client";
 import { Button } from '@react-email/button';
-import { Container, Head, Img, Section, Tailwind, Text } from "@react-email/components";
+import { Container, Head, Hr, Img, Section, Tailwind, Text } from "@react-email/components";
 import { Html } from '@react-email/html';
+import { format } from "date-fns";
 import * as React from 'react';
+import { type ParticipantWithBusAndEvent } from "~/server/api/routers/payment";
 
 interface EventSignUpProps {
-  participant: Participant
-  event: VastraEvent
+  participant: ParticipantWithBusAndEvent;
   cancellationUrl: string;
 }
 
-const mockParticipant: Participant = {
-  id: "testpersson",
-  name: 'Test Testsson',
-  email: 'test@testsson.com',
-  payAmount: 100,
-  phone: '0701234567',
+const mockBus: EventSignUpProps['participant']['bus'] = {
+  id: 'testbus',
+  name: 'Testbuss',
+  eventId: 'testevent',
+  seats: 50,
   createdAt: new Date(),
   updatedAt: new Date(),
-  eventId: 'testevent',
-  youth: false,
-  member: false,
 }
 
-const mockEvent: VastraEvent = {
+const mockEvent: EventSignUpProps['participant']['event'] = {
   id: 'testevent',
   name: 'Testevent',
   description: 'Testevent',
@@ -36,19 +33,44 @@ const mockEvent: VastraEvent = {
   updatedAt: new Date(),
 }
 
+const mockParticipant: EventSignUpProps['participant'] = {
+  id: "testpersson",
+  name: 'Test Testsson',
+  email: 'test@testsson.com',
+  payAmount: 100,
+  phone: '0701234567',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  eventId: 'testevent',
+  youth: false,
+  member: false,
+  bus: mockBus,
+  cancellationDate: new Date(),
+  cancellationToken: 'testtoken',
+  note: 'Testnote',
+  swishPaymentId: 'testpaymentid',
+  busId: 'testbus',
+  event: mockEvent,
+
+}
+
 const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : 'http://localhost:3000';
 
 export const EventSignUp = ({
   participant = mockParticipant,
-  event = mockEvent,
   cancellationUrl
 }: EventSignUpProps) => {
   const {
     name,
     payAmount,
+    note,
+    bus,
+    event
   } = participant;
+
+  const formattedDate = format(event.date, 'yyyy-MM-dd HH:mm');
 
   const {
     name: eventName,
@@ -56,20 +78,33 @@ export const EventSignUp = ({
   return (
     <Tailwind>
       <Html className="bg-slate-900">
-        <Head />
+        <Head>
+          <title>Anmälan till {eventName}</title>
+        </Head>
         <Section className="bg-slate-900">
           <Img src={`${baseUrl}/static/vss_buss.jpg`} width="40%" className="m-auto" />
         </Section>
-        <Section className="bg-slate-800 p-4 mt-4">
+        <Section className="bg-slate-800 p-4 mt-4 text-white">
           <Container>
-            <Text className="text-white text-5xl text-center">Tack för anmälan {name}</Text>
-            <Text className="text-white text-2xl">Du är nu anmäld till eventet</Text>
-            <Text className="text-white text-lg">Du kan avboka din plats genom att klicka på knappen nedan</Text>
+            <Text className="text-5xl text-center">Tack för anmälan</Text>
+            <Hr />
+            <Container>
+              <Text className="text-lg">Event: {eventName}</Text>
+              <Text className="text-lg">Namn: {name}</Text>
+              {bus && (
+                <Text className="text-lg">Buss: {bus.name}</Text>
+              )}
+              <Text className="text-lg">Datum: {formattedDate}</Text>
+              <Text className="text-lg">Pris: {payAmount} kr</Text>
+              {note && <Text className="text-lg">Övrigt: {note}</Text>}
+            </Container>
+            <Hr />
+            <Text className="text-lg">Du kan avboka din plats genom att klicka på knappen nedan</Text>
             <Button
               pX={20}
               pY={12}
               href={cancellationUrl}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
             >
               Avboka
             </Button>
