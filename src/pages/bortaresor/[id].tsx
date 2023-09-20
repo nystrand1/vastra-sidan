4cb32b5ca9c6ac82878c5a2c89daa69d00b14fd0
Filip Nystrand <filip.nystrand@gmail.com>
2023-09-20 22:06:07 +0200

    desktop styling

--- src/pages/bortaresor/[id].tsx
+++ src/pages/bortaresor/[id].tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { useRouter } from "next/router";
 import { AwayGameRules } from "~/components/atoms/AwayGameRules/AwayGameRules";
 import { AwayGameForm } from "~/components/common/AwayGameForm/AwayGameForm";
@@ -12,15 +13,18 @@ export const BusPage = () => {
 
 
   return (
-    <div className="flex flex-col items-center justify-center gap-12">
-      <h1 className="text-center text-4xl md:text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+    <div className="flex flex-col items-center justify-center gap-6">
+      <h1 className="text-center text-4xl md:text-5xl font-extrabold tracking-tight text-white sm:text-5xl">
         {game.name}
       </h1>
+      <p className="text-3xl">
+        Bussen avgår {format(game.date, "hh:mm")}
+      </p>
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-12 md:col-span-5 order-2 md:order-1">
           <AwayGameRules />
         </div>
-        <div className="col-span-12 md:col-span-7 lg:col-span-4 lg:col-start-8 order-1 md:order-2">
+        <div className="col-span-12 md:col-span-7 lg:col-span-4 lg:col-start-8 order-1 md:order-2 pb-6 md:pb-0">
           <AwayGameForm />
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,28 @@ import { createSSRHelper } from "~/utils/createSSRHelper";
 export default function Home() {
   const { data: awayGames, isLoading: isLoadingAwayGames } = api.public.getAwayGames.useQuery();
 
+  const awayGamesTest = awayGames;
+
   return (
     <>
       <Head>
         <title>Västra Sidan Events</title>
       </Head>
         <div className="flex flex-col items-center justify-center gap-12">
-          <h1 className="text-5xl font-extrabold text-white sm:text-[5rem]">
+          <h1 className="text-[2.3rem] md:text-[4rem] lg:text-[5rem] font-extrabold text-white">
             Västra Sidan Events
           </h1>
           <div className="grid grid-cols-12 gap-4 md:gap-8 text-black w-full">
             {isLoadingAwayGames && <p className="text-white">Laddar events...</p>}
-            {awayGames?.map((game) => (
-              <div key={game.id} className="col-span-12 md:col-span-4">
+            {awayGamesTest?.map((game, index) => {
+              let centerClass = "";
+              if (index === 0) {
+                centerClass = awayGamesTest.length === 1 ? "md:col-start-4 lg:col-start-5" 
+                  : awayGamesTest.length === 2 ? "md:col-start-1 lg:col-start-3"
+                  : "";
+              }
+              return (
+                <div key={game.id} className={`col-span-12 md:col-span-6 lg:col-span-4 w-full ${centerClass}`}>
                 <Card
                   title={game.name}
                   link={`${PATHS.awayGames}${game.id}`}
@@ -32,7 +41,8 @@ export default function Home() {
                   />
                 </Card>
               </div>
-            ))}
+              )
+            })}
             </div>
         </div>
     </>
