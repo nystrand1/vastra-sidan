f6e7662151db3b22350e5872dcc50ec8cb944026
Filip Nystrand <filip.nystrand@gmail.com>
2023-09-25 18:45:51 +0200

    Make signup button more prominent

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -2,7 +2,7 @@ export const Button = ({ children, onClick, className, disabled, type, ...props 
   return (
     <button
       onClick={onClick}
-      className={`text-slate-100 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ${className ?? ''}`}
+      className={`text-slate-100 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:bg-slate-500 disabled:hover:bg-slate-500 ${className ?? ''}`}
       disabled={disabled}
       type={type}
       {...props}
