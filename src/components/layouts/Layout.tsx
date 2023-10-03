f421e15cfaa56403374a972de5ee0910d9cad7ad
Filip Nystrand <filip.nystrand@gmail.com>
2023-10-03 21:48:55 +0200

    Image background

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { type PropsWithChildren } from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
 
 const Layout = ({ children } : PropsWithChildren) => {
   return (
@@ -8,7 +9,13 @@ const Layout = ({ children } : PropsWithChildren) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="fixed w-screen h-screen bg-gradient-to-b from-[#051b97] to-[#000000]"/>
+      <div className="z-10 fixed w-screen h-screen bg-slate-900 opacity-70"/>
+      <div className="fixed w-screen h-screen bg-right md:hidden">
+        <Image objectFit='cover' src="/static/background_mobile.webp" fill alt="Uppsala det är staden"/>
+      </div>
+      <div className="fixed w-screen h-screen bg-right hidden md:block">
+        <Image objectFit='cover' src="/static/background.webp" fill alt="Uppsala det är staden"/>
+      </div>
       <main className="flex min-h-screen flex-col items-center justify-center z-10 relative">
         <div className="container p-4 md:p-8">
           {children}
