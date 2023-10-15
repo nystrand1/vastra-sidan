5feafb54ff2768e0bb2a239aae8b4e5a5af781cf
Filip Nystrand <filip.nystrand@gmail.com>
2023-10-15 12:36:00 +0200

    Cron job for memberships

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -13,10 +13,10 @@ const Layout = ({ children } : PropsWithChildren) => {
       <Navigation />
       <div className="z-10 fixed w-screen h-screen bg-slate-900 opacity-70"/>
       <div className="fixed w-screen h-screen bg-right md:hidden">
-        <Image objectFit='cover' src="/static/background_mobile.webp" fill alt="Uppsala det är staden"/>
+        <Image style={{objectFit: 'cover'}} src="/static/background_mobile.webp" fill alt="Uppsala det är staden"/>
       </div>
       <div className="fixed w-screen h-screen bg-right hidden md:block">
-        <Image objectFit='cover' src="/static/background.webp" fill alt="Uppsala det är staden"/>
+        <Image style={{objectFit: 'cover'}} src="/static/background.webp" fill alt="Uppsala det är staden"/>
       </div>
       <main className="flex min-h-screen flex-col items-center justify-center z-10 relative">
         <div className="container p-4 md:p-8 mt-16">
