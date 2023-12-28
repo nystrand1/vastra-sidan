68f58cd98d39885b8871f9f565233088bac6e1d9
Filip Nystrand <filip.nystrand@gmail.com>
2023-12-28 18:49:59 +0100

    News landing page cleanup

--- src/pages/nyheter/index.tsx
+++ src/pages/nyheter/index.tsx
@@ -22,22 +22,23 @@ export default function NewsPage() {
       </Head>
       <div>
         <h1 className="text-center mb-4 text-5xl">Nyheter</h1>
-        <div className="grid md:grid-cols-3 md:w-10/12 m-auto gap-4 md:items-stretch flex-wrap justify-center">
+        <div className="grid md:w-10/12 m-auto gap-4 justify-center">
           {news?.map(({ slug, title, date, excerpt, image, author  }) => (
-            <Card title={title} key={slug} contentClassName="justify-start">
-              <div className="relative aspect-video rounded-md overflow-hidden">
+            <Card key={slug} titleClassName="text-center" contentClassName="md:!grid md:!grid-cols-2 md:gap-4 md:space-y-0">
+              <div className="relative aspect-video rounded-md overflow-hidden md:order-2 md:my-auto">
                 {image ? (
                   <Image className="object-cover" src={image.sourceUrl} alt={image.altText} fill />
                 ) : (
                   <Image className="object-contain" src="/static/logo.png" alt="Västra Sidan logo" fill />
                 )}
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col space-y-4">
+                <h2 className="text-2xl font-semibold text-center">{title}</h2>
                 <p className="text-gray-400">{date}</p>
-                <p className="text-gray-400">{author}</p>
+                <p className="text-gray-400 !mt-0">{author}</p>
+                <Wysiwyg content={excerpt} />
+                <ButtonLink className="justify-end" href={`/nyheter/${slug}`}>Läs mer</ButtonLink>
               </div>
-              <Wysiwyg content={excerpt} />
-              <ButtonLink className="justify-end" href={`/nyheter/${slug}`}>Läs mer</ButtonLink>
             </Card>
           ))}
         </div>
