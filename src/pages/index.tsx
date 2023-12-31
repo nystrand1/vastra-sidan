4434a1327a38098dd299fcd0f8f09034b3eb1dc0
Filip Nystrand <filip.nystrand@gmail.com>
2023-12-31 10:44:30 +0100

    Start page route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
-import { format, isAfter } from "date-fns";
+import { format } from "date-fns";
 import Head from "next/head";
 import Card from "~/atoms/CardLink/CardLink";
 import { Progressbar } from "~/atoms/Progressbar/Progressbar";
 import { Button } from "~/components/atoms/Button/Button";
+import { ButtonLink } from "~/components/atoms/ButtonLink/ButtonLink";
 import { api } from "~/utils/api";
 import { PATHS } from "~/utils/constants";
 import { createSSRHelper } from "~/utils/createSSRHelper";
 
 export default function Home() {
-  const { data: awayGames, isLoading: isLoadingAwayGames } = api.public.getAwayGames.useQuery();
-  
+  const { data: startPage } = api.public.getStartPage.useQuery();
+
+  if (!startPage) {
+    return null;
+  }
+
+  const { upcomingEvent, memberCount } = startPage;
+
   return (
     <>
       <Head>
@@ -19,48 +26,42 @@ export default function Home() {
           <h1 className="text-[2.3rem] md:text-[4rem] lg:text-[5rem] font-extrabold text-white">
             Västra Sidan
           </h1>
-          <div className="grid grid-cols-12 gap-4 md:gap-8 text-black w-full">
-            {isLoadingAwayGames && <p className="text-white col-span-12 text-center">Laddar resor...</p>}
-            {awayGames?.map((game, index) => {
-              let centerClass = "";
-              const gameExpired = isAfter(new Date(), game.date);
-              if (index === 0) {
-                centerClass = awayGames.length === 1 ? "md:col-start-4 xl:col-start-5" 
-                  : awayGames.length === 2 ? "md:col-start-1 xl:col-start-3"
-                  : "";
-              }
-              return (
-                <div key={game.id} className={`col-span-12 md:col-span-6 xl:col-span-4 w-full ${centerClass}`}>
+          <div className={`grid md:grid-cols-2 gap-4 md:gap-8 text-black w-full md:w-8/12`}>
+            <Card 
+              title="Antal medlemmar"
+              className="w-full first-letter:space-y-0 md:h-52"
+              contentClassName="flex flex-col justify-between h-full"
+            >
+              <p className="text-4xl">{memberCount}</p>
+              <ButtonLink href="https://apply.cardskipper.se/pxvo" target="_blank" className="w-full">Bli medlem</ButtonLink>
+            </Card>
+              {upcomingEvent && upcomingEvent.id && (
                 <Card
-                  title={game.name}
-                  link={`${PATHS.awayGames}${game.id}`}
+                  title="Nästa bortaresa"
+                  link={`${PATHS.awayGames}${upcomingEvent.id}`}
                 >
                   <div className="space-y-1">
-                    <p className="text-md font-semibold">Bussen avgår {format(game.date, "HH:mm")}</p>
-                    <p className="text-sm">Pris vuxen medlem: {game.memberPrice} kr</p>
-                    <p className="text-sm">Pris vuxen icke medlem: {game.defaultPrice} kr</p>
-                    <p className="text-sm">Pris ungdom medlem: {game.youthMemberPrice} kr</p>
-                    <p className="text-sm">Pris ungdom icke medlem: {game.youthPrice} kr</p>
+                    <p className="text-lg font-semibold">{upcomingEvent.name}</p>
+                    <p className="text-md font-semibold">Bussen avgår {format(upcomingEvent.date, "HH:mm")}</p>
+                    <p className="text-sm">Pris vuxen medlem: {upcomingEvent.memberPrice} kr</p>
+                    <p className="text-sm">Pris vuxen icke medlem: {upcomingEvent.defaultPrice} kr</p>
+                    <p className="text-sm">Pris ungdom medlem: {upcomingEvent.youthMemberPrice} kr</p>
+                    <p className="text-sm">Pris ungdom icke medlem: {upcomingEvent.youthPrice} kr</p>
                   </div>
                   <Progressbar
                     label="Antal anmälda"
-                    maxValue={game.maxSeats}
-                    currentValue={game.bookedSeats}
+                    maxValue={upcomingEvent.maxSeats}
+                    currentValue={upcomingEvent.bookedSeats}
                   />
-                  {game.bookedSeats < game.maxSeats && (
+                  {upcomingEvent.bookedSeats < upcomingEvent.maxSeats && (
                     <Button>Till anmälan</Button>
                   )}
-                  {game.bookedSeats >= game.maxSeats && !gameExpired && (
+                  {upcomingEvent.bookedSeats >= upcomingEvent.maxSeats && (
                     <Button disabled>Fullbokat</Button>
                   )}
-                  {gameExpired && (
-                    <Button disabled>Resan har redan varit</Button>
-                  )}
+
                 </Card>
-              </div>
-              )
-            })}
-            {!isLoadingAwayGames && awayGames?.length === 0 && <p className="text-white text-center col-span-12">Inga planerade resor just nu</p>}
+              )}
             </div>
         </div>
     </>
