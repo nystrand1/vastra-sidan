79172bba06e976d91e1a5148d368b811ae2c44d5
Filip Nystrand <filip.nystrand@gmail.com>
2023-12-31 10:44:09 +0100

    Use same button color as IKS

--- src/components/atoms/ButtonLink/ButtonLink.tsx
+++ src/components/atoms/ButtonLink/ButtonLink.tsx
@@ -3,7 +3,7 @@ import Link, { type LinkProps } from "next/link"
 export const ButtonLink = ({className, children, ...props} : LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
   return (
     <Link
-    className={`text-slate-100 text-center focus:ring-4 font-medium rounded-md text-sm px-5 py-2.5 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800 ${className ?? ''}`}
+    className={`text-slate-100 text-center focus:ring-4 font-medium rounded-md text-sm px-5 py-2.5 mb-2 bg-[#1e599f] hover:bg-[#164174] focus:outline-none focus:ring-blue-800 ${className ?? ''}`}
     {...props}
    >
       {children}
