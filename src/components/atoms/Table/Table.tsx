08cd4842451b4736b655739610f2822afabf249a
Filip Nystrand <filip.nystrand@gmail.com>
2024-01-13 12:03:57 +0100

    Add away table to guide

---
+++ src/components/atoms/Table/Table.tsx
@@ -0,0 +1,47 @@
+interface Column {
+  key: string;
+  title: string;
+}
+
+interface Row {
+  id: number;
+  [key: string]: any;
+}
+
+interface TableProps {
+  data: Row[];
+  columns: Column[];
+}
+
+export const Table: React.FC<TableProps> = ({ data, columns }) => {
+  return (
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          {columns.map((column) => (
+            <th
+              key={column.key}
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              {column.title}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {data.map((row) => (
+          <tr key={row.id}>
+            {columns.map((column) => (
+              <td key={column.key} className="px-6 py-4 whitespace-nowrap">
+                {row[column.key]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- src/pages/bortaguiden/[id].tsx
+++ src/pages/bortaguiden/[id].tsx
@@ -1,10 +1,81 @@
+import { type inferRouterOutputs } from "@trpc/server";
 import { type GetStaticPropsContext } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import Card from "~/components/atoms/CardLink/CardLink";
 import { Wysiwyg } from "~/components/atoms/Wysiwyg/Wysiwyg";
+import { type AppRouter } from "~/server/api/root";
 import { api } from "~/utils/api";
 import { createSSRHelper } from "~/utils/createSSRHelper";
+
+type AwayGuide = inferRouterOutputs<AppRouter>['wordpress']['getAwayGuideBySlug']['awayGuide'];
+
+const AwayGuideTable = (awayGuide: AwayGuide) => {
+
+  const rows = [
+    {
+      label: 'Lag:',
+      value: awayGuide.lag
+    },
+    {
+      label: 'Bildad:',
+      value: awayGuide.bildad
+    },
+    {
+      label: 'Sport:',
+      value: awayGuide.sport
+    },
+    {
+      label: 'Meriter:',
+      value: (
+        <p dangerouslySetInnerHTML={{ __html: awayGuide.meriter }}></p>
+      )
+    },
+    {
+      label: 'Hemsida:',
+      value: (
+        <Link className="underline" href={awayGuide.hemsida}>{awayGuide.hemsida}</Link>
+      )
+    },
+    {
+      label: 'Avstånd:',
+      value: awayGuide.avstand,
+    },
+    {
+      label: 'Matcher:',
+      value: awayGuide.matcher,
+    },
+    {
+      label: 'Division:',
+      value: awayGuide.division,
+    },
+    {
+      label: 'Färger:',
+      value: awayGuide.farger
+    }
+  ]
+
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2">
+      <table className="order-2 xl:order-1">
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={row.label} className={`bg-slate-${index % 2 ? '600' : '700'}`}>
+              <td className="font-semibold w-24 p-2">{row.label}</td>
+              <td>{row.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex items-center justify-center order-1 xl:order-2">
+        <Image src={awayGuide.logo.sourceUrl} width={200} height={200} alt={awayGuide.lag} />
+      </div>
+    </div>
+  )
+};
 
 export default function AwayGamePage() {
   const { id } = useRouter().query;
@@ -39,6 +110,7 @@ export default function AwayGamePage() {
           titleAsH1
         >
           <p className="text-gray-400">{date}</p>
+          <AwayGuideTable {...awayGuide} />
           <Wysiwyg content={awayGuideContent.text} />
         </Card>
       </div>
