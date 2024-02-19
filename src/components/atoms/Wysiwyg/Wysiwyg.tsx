3ac326cd4642449652ef16797cf2cb43c519b307
Filip Nystrand <filip.nystrand@gmail.com>
2024-02-19 17:53:43 +0100

    Use next img optimize on prod

--- src/components/atoms/Wysiwyg/Wysiwyg.tsx
+++ src/components/atoms/Wysiwyg/Wysiwyg.tsx
@@ -1,10 +1,37 @@
 
+import { type HTMLReactParserOptions } from 'html-react-parser';
+import parse from "html-react-parser"
+import Image from 'next/image';
+import { env } from '~/env.mjs';
 interface WysiwygProps {
   content: string
 }
 
+// Utilize html-react-parser to parse the content and replace img tags with Next Image components.
+const options: HTMLReactParserOptions = {
+  replace: (domNode) => {
+    const shouldReplace = process.env.VERCEL_ENV === 'production' || env.USE_DEV_MODE === 'true'
+    // Look for an img tag and replace it with Image.
+    if (shouldReplace && 'name' in domNode && 'attribs' in domNode && domNode.name === "img") {
+      const { src, alt, width, height } = domNode.attribs
+      return (
+        <Image
+          src={src || ''}
+          width={Number(width) || 400}
+          height={Number(height) || 400}
+          alt={alt || ''}
+          objectFit="cover"
+        />
+      )
+    }
+  },
+}
+
 export const Wysiwyg = ({ content } : WysiwygProps) => {
+
   return (
-    <div className="[&_p]:mb-4 [&_h3]:mb-4 [&_div]:mb-4 [&_a]:underline" dangerouslySetInnerHTML={{ __html: content }} />
+    <div className="[&_p]:mb-4 [&_h3]:mb-4 [&_div]:mb-4 [&_a]:underline">
+      {parse(content, options)}
+    </div>
   )
 }
