1fc1198bca5e13eec7ff3c6469b74aedc0981c63
Filip Nystrand <filip.nystrand@gmail.com>
2024-12-01 12:13:51 +0100

    fix

--- src/pages/medlem/[memberToken].tsx
+++ src/pages/medlem/[memberToken].tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Button } from "~/components/atoms/Button/Button";
 import { ButtonLink } from "~/components/atoms/ButtonLink/ButtonLink";
 import _MemberCard from "~/components/common/MemberCard/MemberCard";
+import { env } from "~/env.mjs";
 import { api } from "~/utils/api";
 import { createSSRHelper } from "~/utils/createSSRHelper";
 
@@ -13,7 +14,7 @@ export const MemberCard = dynamic(() => Promise.resolve(_MemberCard), {
 });
 
 export const MemberPage = ({ memberToken }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { data, isLoading } = api.member.getMember.useQuery({ memberToken });
+  const { data, isLoading } = api.member.getMember.useQuery({ memberToken }, { enabled: !!memberToken });
   const [flipped, setFlipped] = useState(false);
   if (isLoading) {
     return <div className="flex justify-center">Laddar...</div>
@@ -48,7 +49,12 @@ export const MemberPage = ({ memberToken }: InferGetStaticPropsType<typeof getSt
   return (
     <div>
       <div className="absolute inset-0 w-screen -mt-10 md:mt-0 h-[100vh] md:w-screen md:h-[100vh] m-auto overflow-hidden flex justify-center items-center flex-col">
-        <MemberCard memberName={data.name} flipped={flipped} {...activeMembership} />
+        <MemberCard
+          {...activeMembership}
+          memberName={data.name}
+          flipped={flipped}
+          imageUrl={`${env.NEXT_PUBLIC_WEBSITE_URL}/_next/image?url=${encodeURIComponent(activeMembership.imageUrl)}&w=640&q=75`}
+        />
         <Button className="text-center m-auto bottom-6 absolute w-32" onClick={() => setFlipped(!flipped)}>
           {flipped ? 'Vänd tillbaka' : 'Vänd'}
         </Button>
@@ -62,19 +68,19 @@ export default MemberPage;
 export const getStaticPaths = () => {
   return {
     paths: [],
-    fallback: "blocking",
+    fallback: true,
   }
 }
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const { memberToken } = context.params as { memberToken: string };
-  
+
   if (!memberToken) {
     return {
       notFound: true,
     }
   }
-  
+
   const ssg = await createSSRHelper();
 
   const member = await ssg.member.getMember.fetch({ memberToken });
