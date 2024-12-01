94081adffef179a4c293a9de0e77df0587ae05a5
Filip Nystrand <filip.nystrand@gmail.com>
2024-12-01 13:51:16 +0100

    Fix card rendering

--- src/components/common/MemberCard/MemberCard.tsx
+++ src/components/common/MemberCard/MemberCard.tsx
@@ -3,15 +3,11 @@ import { Physics } from "@react-three/rapier";
 
 import { type inferRouterOutputs } from "@trpc/server";
 import { type AppRouter } from "~/server/api/root";
+import BandAndCard from "./BandAndCard";
 import { useTexture } from "@react-three/drei";
-import dynamic from "next/dynamic";
 
 type ActiveMembership = NonNullable<inferRouterOutputs<AppRouter>['member']['getMember']>['activeMemberships'][number]
 
-const BandAndCard = dynamic(() => import('~/components/common/MemberCard/BandAndCard'), {
-  ssr: false,
-  loading: () => <div className="flex justify-center">Laddar...</div>
-});
 
 interface MemberCardProps extends ActiveMembership {
   flipped: boolean
@@ -22,13 +18,11 @@ export default function MemberCard({ flipped, memberName, name, imageUrl }: Memb
   useTexture.preload(imageUrl);
 
   return (
-    <>
-      <Canvas camera={{ position: [0, 0, 13], fov: 25 }} className="touch-none">
-        <ambientLight intensity={Math.PI} />
-        <Physics interpolate gravity={[0, -30, 0]} timeStep={1 / 120}>
-          <BandAndCard name={memberName} memberType={name} flipped={flipped} />
-        </Physics>      
-      </Canvas>
-    </>
+    <Canvas camera={{ position: [0, 0, 13], fov: 25 }} className="touch-none lg:touch-auto">
+      <ambientLight intensity={Math.PI} />
+      <Physics interpolate gravity={[0, -30, 0]} timeStep={1 / 120}>
+        <BandAndCard name={memberName} memberType={name} flipped={flipped} imageUrl={imageUrl} />
+      </Physics>      
+    </Canvas>
   )
 }
