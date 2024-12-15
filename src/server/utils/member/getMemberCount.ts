4b9b428dee65085e8b48ad94a63790a8339d9a44
Filip Nystrand <filip.nystrand@gmail.com>
2024-12-15 14:27:29 +0100

    Today should be between start and end

--- src/server/utils/member/getMemberCount.ts
+++ src/server/utils/member/getMemberCount.ts
@@ -2,20 +2,22 @@ import { prisma } from "~/server/db"
 
 export const getMemberCount = async () => {
   const members = await prisma.member.findMany({    
-    select: {
-      _count: true,
-    },
     where: {
       memberships: {
         some: {
           endDate: {
             gte: new Date()
-          },          
+          },  
+          startDate: {
+            lte: new Date()
+          }        
         },       
       },
+    },
+    include: {
+      memberships: true,
     }
   })
-
   if (!members) {
     return 0
   }
