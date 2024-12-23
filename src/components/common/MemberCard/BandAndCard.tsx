3167ee13475f8e19d02013aa1867b6edba7bfb34
Filip Nystrand <filip.nystrand@gmail.com>
2024-12-23 14:17:49 +0100

    minor card fixes

--- src/components/common/MemberCard/BandAndCard.tsx
+++ src/components/common/MemberCard/BandAndCard.tsx
@@ -49,7 +49,12 @@ export default function BandAndCard({
   const ang = new THREE.Vector3(0, 0, 0);
   const rot = new THREE.Vector3(0, 0, 0);
   const dir = new THREE.Vector3(0, 0, 0);
-  const segmentProps = { type: 'dynamic', canSleep: true, angularDamping: 2, linearDamping: 2 }
+  const segmentProps = { 
+    type: 'dynamic', 
+    canSleep: true, 
+    angularDamping: 2, 
+    linearDamping: 2,
+   }
   const { nodes, materials } = useGLTF(textureUrl);
   const texture = useTexture('/static/membercard_band.jpg');
   const { width, height } = useThree((state) => state.size)
@@ -63,7 +68,7 @@ export default function BandAndCard({
   useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1.2]) // prettier-ignore
   useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1.2]) // prettier-ignore
   useSphericalJoint(j3, card, [[0, 0, 0], [0, 1.45, 0]]) // prettier-ignore
-
+   
   useEffect(() => {
     if (hovered) {
       document.body.style.cursor = dragged ? 'grabbing' : 'grab'
@@ -81,7 +86,7 @@ export default function BandAndCard({
   }, [card, flipped])
 
   useFrame((state, delta) => {
-    if (typeof dragged === 'object') {
+    if (typeof dragged === 'object' && dragged) {
       vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera)
       dir.copy(vec).sub(state.camera.position).normalize()
       vec.add(dir.multiplyScalar(state.camera.position.length()))
@@ -104,17 +109,17 @@ export default function BandAndCard({
       // Tilt it back towards the screen
       ang.copy(card.current!.angvel())
       rot.copy(card.current!.rotation())
-      const settleYFactor = ang.y - rot.y * 0.25
-      card.current!.setAngvel({ x: ang.x, y: settleYFactor, z: ang.z }, false)
+
+      card.current!.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z }, false);
     }
   })
 
   curve.curveType = 'chordal'
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-  console.log('materials', materials);
+
   return (
     <>
-      <group position={[0, 4, 0]} dispose={null}>
+      <group position={[0, 4, 0]}>
         <RigidBody ref={fixed} {...segmentProps} type="fixed" />
         <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps} type="dynamic">
           <BallCollider args={[0.1]} />
@@ -128,7 +133,6 @@ export default function BandAndCard({
         <RigidBody position={[2, 0, 0]} ref={card} {...segmentProps} type={dragged ? 'kinematicPosition' : 'dynamic'}>
           <CuboidCollider args={[0.8, 1.125, 0.01]} />
           <group
-            dispose={null}  
             scale={2.25}
             position={[0, -1.2, 0]}
             rotation={[0, flipped ? Math.PI : 0, 0]}
@@ -144,39 +148,37 @@ export default function BandAndCard({
               event.setPointerCapture(e.pointerId);
               drag(new THREE.Vector3().copy(e.point).sub(vec.copy(card.current!.translation())));
             }}>
-            <group dispose={null} position={[0, 0.5, 0]} rotation={[0, 0, 0]}>
-              <mesh geometry={(nodes.card as THREE.Mesh).geometry} position={[0, -0.5, 0]} material={materials.metal} material-roughness={0.3}>
-                <meshPhysicalMaterial map={(materials.base as THREE.MeshStandardMaterial)?.map} map-anisotropy={16} clearcoat={1} clearcoatRoughness={0.15} roughness={0.3} metalness={0.5} />
-                <Center scale={0.5} position={[0.5, 0.45, 0]}>
-                    <Html
-                      center
-                      transform
-                      className={twMerge('text-white text-right whitespace-nowrap flex w-36 m-auto transition-opacity opacity-0 delay-300', flipped ? 'opacity-100 animate-pulse' : '')}
-                      scale={0.2}
-                      position={[-0.785, -0.35, 0]}
-                      rotation={[0, Math.PI, 0]}>
-                      {name}
-                    </Html>
-                    <Html
-                      center
-                      transform
-                      className={twMerge('text-white text-right whitespace-nowrap flex w-36 m-auto transition-opacity opacity-0 delay-300', flipped ? 'opacity-100 animate-pulse' : '')}
-                      scale={0.15}
-                      position={[-0.7, -0.5, 0]}
-                      rotation={[0, Math.PI, 0]}>
-                      {memberType}
-                    </Html>
-                  </Center>
-              </mesh>                
-            </group>
-            <mesh dispose={null} geometry={'geometry' in nodes.clip! ? nodes.clip.geometry as THREE.BufferGeometry<THREE.NormalBufferAttributes> : undefined} material={materials.metal} material-roughness={0.3} />
-            <mesh dispose={null} geometry={'geometry' in nodes.clamp! ? nodes.clamp.geometry as THREE.BufferGeometry<THREE.NormalBufferAttributes> : undefined} material={materials.metal} />
+            <mesh geometry={(nodes.card as THREE.Mesh).geometry} position={[0, 0, 0]} material={materials.metal} material-roughness={0.3}>
+              <meshPhysicalMaterial map={(materials.base as THREE.MeshStandardMaterial)?.map} map-anisotropy={16} clearcoat={1} clearcoatRoughness={0.15} roughness={0.3} metalness={0.5} />
+              <Center scale={0.5} position={[0.5, 0.45, 0]}>
+                <Html
+                  center
+                  transform
+                  className={twMerge('text-white text-right whitespace-nowrap flex w-36 m-auto transition-opacity opacity-0 delay-300', flipped ? 'opacity-100 animate-pulse' : '')}
+                  scale={0.2}
+                  position={[-0.785, -0.35, 0]}
+                  rotation={[0, Math.PI, 0]}>
+                  {name}
+                </Html>
+                <Html
+                  center
+                  transform
+                  className={twMerge('text-white text-right whitespace-nowrap flex w-36 m-auto transition-opacity opacity-0 delay-300', flipped ? 'opacity-100 animate-pulse' : '')}
+                  scale={0.15}
+                  position={[-0.7, -0.5, 0]}
+                  rotation={[0, Math.PI, 0]}>
+                  {memberType}
+                </Html>
+              </Center>
+            </mesh>
+            <mesh geometry={'geometry' in nodes.clip! ? nodes.clip.geometry as THREE.BufferGeometry<THREE.NormalBufferAttributes> : undefined} material={materials.metal} material-roughness={0.3} />
+            <mesh geometry={'geometry' in nodes.clamp! ? nodes.clamp.geometry as THREE.BufferGeometry<THREE.NormalBufferAttributes> : undefined} material={materials.metal} />
           </group>
         </RigidBody>
       </group>
-      <mesh ref={band} dispose={null}>
+      <mesh ref={band}>
         <meshLineGeometry />
-        <meshLineMaterial color="white" depthTest={false} resolution={resolution} useMap={1} map={texture} repeat={repeat} lineWidth={1} />
+        <meshLineMaterial color="white" resolution={resolution} useMap={1} map={texture} repeat={repeat} lineWidth={1} />
       </mesh>
     </>
   )
