5ca20cccddc1aee9f369cc2a792d49c05771143b
Filip Nystrand <filip.nystrand@gmail.com>
2025-02-05 21:24:50 +0100

    Add badge to cardlink

--- src/components/atoms/CardLink/CardLink.tsx
+++ src/components/atoms/CardLink/CardLink.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { type PropsWithChildren } from "react";
 import { twMerge } from 'tailwind-merge';
+import { Badge } from '~/components/ui/badge';
 import { Card, CardContent, CardHeader } from '~/components/ui/card'
 
 interface CardLinkProps extends PropsWithChildren {
@@ -11,18 +12,22 @@ interface CardLinkProps extends PropsWithChildren {
   contentClassName?: string;
   titleClassName?: string;
   titleAsH1?: boolean;
+  badge?: string;
 }
 
-const CardLink = ({ title, href, target, className, children, contentClassName, titleClassName, titleAsH1 }: CardLinkProps) => {
+const CardLink = ({ title, badge, href, target, className, children, contentClassName, titleClassName, titleAsH1 }: CardLinkProps) => {
   const card = (
     <Card className={`overflow-hidden ${href ? 'cursor-pointer' : ''} ${className || ''}`}>
-      <CardHeader>
+      <CardHeader className='flex flex-row justify-between'>
         {title && (
           titleAsH1 ? (
               <h1 className={`text-xl font-semibold ${titleClassName || ''}`}>{title}</h1>
             ) : (
               <h2 className={`text-xl font-semibold ${titleClassName || ''}`}>{title}</h2>
             )
+        )}
+        {badge && (
+          <Badge className='w-fit'>{badge}</Badge>
         )}
       </CardHeader>
       <CardContent className={twMerge("space-y-4", contentClassName)}>
