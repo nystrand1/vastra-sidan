d0dcaafe45276d4f2cc131a54af813a62cb7bf25
Filip Nystrand <filip.nystrand@gmail.com>
2025-12-10 21:40:22 +0100

    Prep for 2026 membership

--- src/server/api/routers/public.ts
+++ src/server/api/routers/public.ts
@@ -87,6 +87,9 @@ export const publicRouter = createTRPCRouter({
       where: {
         endDate: {
           gt: new Date()
+        },
+        startDate: {
+          lte: new Date()
         }
       },
       select: {
