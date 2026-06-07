ee4491fc6ae9018beeabd98f94da811a8011db24
Filip Nystrand <filip.nystrand@gmail.com>
2026-06-07 19:34:08 +0200

    Remove resend

--- src/server/utils/email/sendMemberConfirmationEmail.ts
+++ src/server/utils/email/sendMemberConfirmationEmail.ts
@@ -2,7 +2,6 @@ import { type Member, type Membership } from "@prisma/client";
 import { render } from "@react-email/components";
 import MemberSignup from "~/components/emails/MemberSignUp";
 import { env } from "~/env.mjs";
-import { resend } from "../../resend";
 import { sendSesEmail } from "./sendSesEmail";
 
 export const sendMemberConfirmationEmail = async (
@@ -16,28 +15,14 @@ export const sendMemberConfirmationEmail = async (
     phone: member.phone,
   }
 
-  if (env.ENABLE_AWS_SES_EMAILS) {
-    return await sendSesEmail({
-      to: member.email,
-      subject: `Tack för att du blivit medlem i Västra Sidan`,
-      body: await render(MemberSignup({ 
-        member: formattedMember, 
-        memberUrl,
-        membershipType: membership.type,
-        memberImageUrl: membership.imageUrl,
-      }))
-    })
-  }
-
-  return await resend.emails.send({
-    from: `Västra Sidan <${env.BOOKING_EMAIL}>`,
-    to: env.USE_DEV_MODE === "true" ? "filip.nystrand@gmail.com" : member.email,
+  return await sendSesEmail({
+    to: member.email,
     subject: `Tack för att du blivit medlem i Västra Sidan`,
-    react: MemberSignup({ 
+    body: await render(MemberSignup({ 
       member: formattedMember, 
       memberUrl,
       membershipType: membership.type,
       memberImageUrl: membership.imageUrl,
-    })
-  });
+    }))
+  })
 };
